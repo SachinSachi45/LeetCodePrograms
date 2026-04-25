8e3cb14d2789f067f16dd3a6ee479beb58f1ba56
Sachin <sachincssachincs8@gmail.com>
2026-04-25 13:01:28 +0530

    commited

---
+++ src/lc/Problems/$80RemoveDuplicatesFromSortedArray2.java
@@ -0,0 +1,17 @@
+package lc.Problems;
+public class $80RemoveDuplicatesFromSortedArray2 {
+public static void main(String[] args) {
+	int nums[] = {1,1,2,2,2,2,3,3,4,4};
+	System.out.println(removeDuplicates(nums));
+}
+
+private static int removeDuplicates(int[] nums) {
+	int i=0;
+    for(int n:nums){
+        if(i<2 || n!=nums[i-2]){
+            nums[i++]=n;
+        }
+    }
+    return i;
+}
+}
