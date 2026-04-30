360e727d0cc793b9fbe67ffa3dff01a66dc8b19f
Sachin <sachincssachincs8@gmail.com>
2026-04-30 12:43:28 +0530

    commited

---
+++ src/lc/Problems/$151ReverseWordsInString.java
@@ -0,0 +1,19 @@
+package lc.Problems;
+
+public class $151ReverseWordsInString {
+	public static void main(String[] args) {
+		String s="  Hello  World  ";
+		System.out.println(reverseWords(s));
+	}
+
+	private static String reverseWords(String s) {
+		String[] words=s.split(" +");
+		StringBuilder sb= new StringBuilder();
+		for(int i=words.length-1;i>=0;i--) {
+			sb.append(words[i]);
+			sb.append(" ");
+		}
+		return sb.toString().trim();
+	}
+
+}
